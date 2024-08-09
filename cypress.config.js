adad656acc6d6924e4eda2b016a4afda65d7a749
Tuna GÜL <63519095+tunywalker@users.noreply.github.com>
2024-08-10 01:58:16 +0300

    # cypress cloud integrration and output done.

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+
+  projectId: "zec6sa",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
